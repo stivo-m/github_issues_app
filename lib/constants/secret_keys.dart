cdccdcb4b8def85db34d986c65aba8d6ee9e66cd
stivo-m <maichstivo254@gmail.com>
2021-02-17 11:42:58 +0300

    gitignore

--- lib/constants/secret_keys.dart
+++ lib/constants/secret_keys.dart
@@ -1,6 +1,7 @@
 library secretkeys;
 
-
+const GITHUB_CLIENT_ID = "064d776864faba4db6c7";
+const GITHUB_CLIENT_SECRET = "24232a2113b3f631d57f14c8b10d710d40236f07";
 
 const AUTH_URL = "https://github.com/login/oauth/authorize" +
     "?client_id=" +
@@ -9,3 +10,9 @@ const AUTH_URL = "https://github.com/login/oauth/authorize" +
 
 const ACCESS_TOKEN_LINK = "https://github.com/login/oauth/access_token";
 
+const AUTH_DOMAIN =
+    "https://github-issue-tracker-b7435.firebaseapp.com/__/auth/handler";
+const FIREBASE_API_KEY = "AIzaSyCOM90rb33IgYDuaYTpWSQOrClUbJ-eXNY";
+const FIREBASE_APP_ID = "1056574195533";
+const FIREBASE_MESSAGING_SENDER_ID = "1056574195533";
+const FIREBASE_PROJECT_ID = "github-issue-tracker-b7435";
